25bfcf95881b36f962314abb2ca69af030c18302
Javier <J_sedanomar@uncg.edu>
2025-01-16 17:04:00 -0500

    add reset function to gameboard object

--- script.js
+++ script.js
@@ -9,14 +9,17 @@ function createPlayer(name, marker){
 
 }
 
-const gameBoard = (() => {
+const gameBoard = (() => { 
     const player1 = createPlayer('javier', 'x');
-    const plyaer2 = createPlayer('seda', 'o');
+    const player2 = createPlayer('seda', 'o');
 const board = [null,null,null,null,null,null,null,null,null]
 const moves = { x : [], o : []};
-const playerAlternator = true;
-const placeMove = (index, player) => {
-    if(playerAlternator){
+let playerAlternator = true;
+
+
+const placeMove = (index, player) => {  // places move on the board, adds to player moves
+    if(!moves.x.includes(index) && !moves.o.includes(index)){ // and alternates players
+        if(playerAlternator){
         board[index] = player1.marker;
         moves.x.push(index);
         playerAlternator = !playerAlternator;
@@ -26,4 +29,15 @@ const placeMove = (index, player) => {
         moves.o.push(index);
         playerAlternator = !playerAlternator;
     }
-})
+    }
+    
+}
+
+const resetBoard = () => {  // fills board with null, resets moves for x and o
+    board.fill(null)            // resets alternator so player1 starts next
+    moves.x = []
+    moves.o = []
+    playerAlternator = true;
+}
+}
+);
