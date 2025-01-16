d9af3986ddb625066493e110565a191dff02ed86
Javier <J_sedanomar@uncg.edu>
2025-01-16 16:56:51 -0500

    added the logic for place move and alternating players

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ function createPlayer(name, marker){
 }
 
 const gameBoard = (() => {
+    const player1 = createPlayer('javier', 'x');
+    const plyaer2 = createPlayer('seda', 'o');
 const board = [null,null,null,null,null,null,null,null,null]
 const moves = { x : [], o : []};
+const playerAlternator = true;
+const placeMove = (index, player) => {
+    if(playerAlternator){
+        board[index] = player1.marker;
+        moves.x.push(index);
+        playerAlternator = !playerAlternator;
+}
+    else {
+        board[index] = player2.marker;
+        moves.o.push(index);
+        playerAlternator = !playerAlternator;
+    }
 })
