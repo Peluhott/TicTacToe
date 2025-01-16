184c936e0f70b2eb05945c1a3eadaae6fb86590a
Javier <J_sedanomar@uncg.edu>
2025-01-16 11:36:19 -0500

    added logic to when a moved is made by a player it checks for a win and if there is no win it filters the combinations out

--- script.js
+++ script.js
@@ -37,21 +37,7 @@ const Game = ( () => {
 
     ];
 
-    /* 
-     if(moveIndex = 0){ // a player marks index 0
-    set array[0,1,2] = null }  // it goes throught the array of combinations,
-                                    and removes the combinations at indexes 0,1,2
-
-    
-    
-    if(moveIndex = 4) {
-    set array[2,3,4,5] = null}
    
-    if(moveIndex = 8){
-    set array[6,7] = null}
-
-    after 3 moves it starts checking for winning combinations and it's still removing possible combinations
-    */
 
     let player1Turn = true;
 
@@ -76,28 +62,36 @@ const Game = ( () => {
      const addMoveToPlayer = (player,index) => {
         if(player == 'player1'){
             player1Moves.push(index);
+            if(player1Moves.length = 3){
+                checkForWin(player1WinningMoves,player1Moves)
+            }
+            filterWinningCombinations(index,player2Moves);
         }
         else {
             player2Moves.push(index);
+            if(player12Moves.length = 3){
+                checkForWin(player2WinningMoves,player2Moves)
+            }
+            filterWinningCombinations(index,player1Moves);
         }
      }
 
-     const filterWinningCombinations = (index, array) => {
+     const filterWinningCombinations = (index, opposingPlayerCombinations) => {
         if(index == 0){
-            array[0] = null;
-            array[1] = null;
-            array[2] = null;
+            opposingPlayerCombinations[0] = null;
+            opposingPlayerCombinations[1] = null;
+            opposingPlayerCombinations[2] = null;
         }
         else if(index == 4){
-            array[2] = null;
-            array[3] = null;
-            array[4] = null;
-            array[5] = null;
+            opposingPlayerCombinations[2] = null;
+            opposingPlayerCombinations[3] = null;
+            opposingPlayerCombinations[4] = null;
+            opposingPlayerCombinations[5] = null;
         }
 
         else if(index == 8){
-            array[6] = null;
-            array[7] = null;
+            opposingPlayerCombinations[6] = null;
+            opposingPlayerCombinations[7] = null;
         }
      }
 
@@ -114,17 +108,5 @@ const Game = ( () => {
         }
         return false;
      }
-     /* winning array combinations 
-     0-1-2      0,3,6       0,4,8
-     3,4,5      1,4,7       6,4,2
-     6,7,8      2,5,8       
-     */
-
-     /* after a player makes a move and takes up a spot , i remove all the combinations
-     that can no longer be achieved by the other player in a list, then after each 
-     player has 3 moves minimum , after a player makes a move i check if they have 
-     any 3-pairs of numbers that would make them a winner, that means i also have 
-     to check to see if the array has every spot taken after the move, if it is and 
-     no one has any combination of numbers , then the game ends in a tie and the
-     board resets*/
+    
 });
